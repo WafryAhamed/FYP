500a273218d4aa4d5511a1a06195802c4853dc86
Wafry Ahamed <wafry07@gmail.com>
2025-11-07 23:57:23 +0530

    fix theme

--- auth-frontend/src/pages/Dashboard/AdminDashboard.jsx
+++ auth-frontend/src/pages/Dashboard/AdminDashboard.jsx
@@ -1,5 +1,10 @@
 // AdminDashboard.jsx
 
 import './AdminDashboard.css';
-const AdminDashboard = () => <div><h1>Admin Dashboard</h1><p>Manage users and platform</p></div>;
+const AdminDashboard = () => 
+<div className="admin-card">
+  <h1>Admin Dashboard</h1>
+  <p>Manage users and platform</p>
+</div>
+
 export default AdminDashboard;
